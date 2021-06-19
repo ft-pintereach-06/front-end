8c0474da0a4500603b15d84c0501aea0ac15e781
John Vehmeyer <j.vehmeyer@gmail.com>
2021-06-19 09:39:28 -0500

    created app scaffolding

---
+++ pintereach/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Connected...</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
