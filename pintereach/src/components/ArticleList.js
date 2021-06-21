03d3df3241864fafaf91da58491ad653dac58bc6
John Vehmeyer <j.vehmeyer@gmail.com>
2021-06-21 14:12:17 -0500

    built scaffolding for ArticleList component. linked component to homepage.

---
+++ pintereach/src/components/ArticleList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// import ArticleCard from './ArticleCard.js';
+
+function ArticleList(props) {
+    // const { articles } = props;
+
+    // useEffect on mount?
+
+    return (
+        <div>
+            <h3>ARTICLE LIST</h3>
+            {/* articles.map(article => <ArticleCard key={article.id} article={article}) */}
+        </div>
+    )
+}
+
+// const mapStateToProps = state => {
+//     return {
+
+//     }
+// }
+
+// export default connect(mapStateToProps)(ArticleList);
+
+export default ArticleList;
+
--- pintereach/src/components/Homepage.js
+++ pintereach/src/components/Homepage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import ArticleList from './ArticleList';
+
 function Homepage() {
     return (
         <div>
-           <h1>Homepage Connected</h1> 
+           <h1>Article Board</h1> 
+        <ArticleList />
         </div>
     )
 }
