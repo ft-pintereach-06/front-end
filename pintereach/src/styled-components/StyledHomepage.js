9065405e7717a0131b82e932d2b3578abb5a3919
caralocke <cara.m.locke@gmail.com>
2021-06-24 10:11:38 -0400

    Added a breakpoint to test on the nav bar.

--- pintereach/src/styled-components/StyledHomepage.js
+++ pintereach/src/styled-components/StyledHomepage.js
@@ -4,7 +4,29 @@ import styled from 'styled-components'
 const StyledHome = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;    
+    align-items: center;
+
+
+    button {
+        font-weight: 600;
+        text-align: center;
+        font-size: 1.1rem;
+        background-color: #e8eeef;
+        color: #242943;
+        border: none;
+        border-radius: 5px;
+        margin: .5em auto;
+        padding: 1%;
+        cursor: pointer;
+        display: block;
+      
+        &:hover {
+            background-color: #fdbb2d;
+            color: #ffffff;
+            transition: all .4s ease-in-out;
+        }
+        transition: all .4s ease-in-out;
+    }  
 `
 export const StyledHomeCard = ({children}) => {
     return <StyledHome>{children}</StyledHome>;
